02f98b394c2e6c36d56ae47e716bedc200b907b9
Ross Merrigan <email@rossmerrigan.com>
2017-03-03 22:41:37 +0000

    cleanup

--- uniformCostSearch.go
+++ uniformCostSearch.go
@@ -44,12 +44,8 @@ func UniformCostSearch(g *Graph, start *Vertex, goal *Vertex) ([]*Vertex, error)
 
 		for _, e := range node.Edges() {
 			if _, ok := explored[e.id]; !ok {
-				if n, ok := g.Find(e.id); ok {
-					frontier = append(frontier, &path{append(p.Vertices, n), p.Cost + e.weight})
-				} else {
-					return nil, errors.New("Vertex not found " + e.id)
-				}
-
+				n := g.Find(e.id)
+				frontier = append(frontier, &path{append(p.Vertices, n), p.Cost + e.weight})
 			}
 		}
 	}
@@ -69,66 +65,6 @@ func (f frontier) Swap(i, j int)          { f[i], f[j] = f[j], f[i] }
 func (f frontier) Less(i, j int) bool     { return f[i].Cost < f[j].Cost }
 func (f frontier) pop() (*path, frontier) { return f[0], f[1:] }
 
-func AustraliaGraph() *Graph {
-	drw1 := &Edge{id: "CNS", weight: 30}
-	drw2 := &Edge{id: "ASP", weight: 15}
-	drw3 := &Edge{id: "PER", weight: 48}
-	drw := &Vertex{id: "DRW", edges: Edges{drw1, drw2, drw3}}
-
-	cns1 := &Edge{id: "DRW", weight: 30}
-	cns2 := &Edge{id: "ASP", weight: 24}
-	cns3 := &Edge{id: "BNE", weight: 22}
-	cns := &Vertex{id: "CNS", edges: Edges{cns1, cns2, cns3}}
-
-	asp1 := &Edge{id: "DRW", weight: 15}
-	asp2 := &Edge{id: "CNS", weight: 24}
-	asp3 := &Edge{id: "BNE", weight: 31}
-	asp4 := &Edge{id: "CBR", weight: 15}
-	asp5 := &Edge{id: "ADL", weight: 15}
-	asp := &Vertex{id: "ASP", edges: Edges{asp1, asp2, asp3, asp4, asp5}}
-
-	bne1 := &Edge{id: "CNS", weight: 22}
-	bne2 := &Edge{id: "ASP", weight: 31}
-	bne3 := &Edge{id: "SYD", weight: 9}
-	bne := &Vertex{id: "BNE", edges: Edges{bne1, bne2, bne3}}
-
-	syd1 := &Edge{id: "BNE", weight: 9}
-	syd2 := &Edge{id: "MEL", weight: 12}
-	syd3 := &Edge{id: "CBR", weight: 4}
-	syd := &Vertex{id: "SYD", edges: Edges{syd1, syd2, syd3}}
-
-	cbr1 := &Edge{id: "MEL", weight: 6}
-	cbr2 := &Edge{id: "SYD", weight: 4}
-	cbr3 := &Edge{id: "ASP", weight: 15}
-	cbr := &Vertex{id: "CBR", edges: Edges{cbr1, cbr2, cbr3}}
-
-	mel1 := &Edge{id: "SYD", weight: 12}
-	mel2 := &Edge{id: "CBR", weight: 6}
-	mel3 := &Edge{id: "ADL", weight: 8}
-	mel := &Vertex{id: "MEL", edges: Edges{mel1, mel2, mel3}}
-
-	adl1 := &Edge{id: "MEL", weight: 8}
-	adl2 := &Edge{id: "ASP", weight: 15}
-	adl3 := &Edge{id: "PER", weight: 32}
-	adl := &Vertex{id: "ADL", edges: Edges{adl1, adl2, adl3}}
-
-	per1 := &Edge{id: "ADL", weight: 32}
-	per2 := &Edge{id: "DRW", weight: 48}
-	per := &Vertex{id: "PER", edges: Edges{per1, per2}}
-	vertices := make(map[string]*Vertex)
-	vertices[drw.id] = drw
-	vertices[cns.id] = cns
-	vertices[asp.id] = asp
-	vertices[bne.id] = bne
-	vertices[syd.id] = syd
-	vertices[cbr.id] = cbr
-	vertices[mel.id] = mel
-	vertices[adl.id] = adl
-	vertices[per.id] = per
-	g := &Graph{Vertices: vertices}
-	return g
-}
-
 type Vertex struct {
 	id    string
 	edges Edges
@@ -147,9 +83,8 @@ type Graph struct {
 	Vertices map[string]*Vertex
 }
 
-func (g *Graph) Find(ID string) (*Vertex, bool) {
-	v, b := g.Vertices[ID]
-	return v, b
+func (g *Graph) Find(ID string) *Vertex {
+	return g.Vertices[ID]
 }
 
 type Edges []*Edge
--- uniformCostSearch_test.go
+++ uniformCostSearch_test.go
@@ -5,8 +5,8 @@ import "strconv"
 
 func Test_UniformCostSearch(t *testing.T) {
 	g := AustraliaGraph()
-	start, _ := g.Find("SYD")
-	goal, _ := g.Find("PER")
+	start := g.Find("SYD")
+	goal := g.Find("PER")
 	result, err := UniformCostSearch(g, start, goal)
 
 	if err != nil {
@@ -37,4 +37,64 @@ func Test_UniformCostSearch(t *testing.T) {
 	if result[4].id != "PER" {
 		t.Fatalf("Expected count to be %s but was %s", "PER", result[4].id)
 	}
+}
+
+func AustraliaGraph() *Graph {
+	drw1 := &Edge{id: "CNS", weight: 30}
+	drw2 := &Edge{id: "ASP", weight: 15}
+	drw3 := &Edge{id: "PER", weight: 48}
+	drw := &Vertex{id: "DRW", edges: Edges{drw1, drw2, drw3}}
+
+	cns1 := &Edge{id: "DRW", weight: 30}
+	cns2 := &Edge{id: "ASP", weight: 24}
+	cns3 := &Edge{id: "BNE", weight: 22}
+	cns := &Vertex{id: "CNS", edges: Edges{cns1, cns2, cns3}}
+
+	asp1 := &Edge{id: "DRW", weight: 15}
+	asp2 := &Edge{id: "CNS", weight: 24}
+	asp3 := &Edge{id: "BNE", weight: 31}
+	asp4 := &Edge{id: "CBR", weight: 15}
+	asp5 := &Edge{id: "ADL", weight: 15}
+	asp := &Vertex{id: "ASP", edges: Edges{asp1, asp2, asp3, asp4, asp5}}
+
+	bne1 := &Edge{id: "CNS", weight: 22}
+	bne2 := &Edge{id: "ASP", weight: 31}
+	bne3 := &Edge{id: "SYD", weight: 9}
+	bne := &Vertex{id: "BNE", edges: Edges{bne1, bne2, bne3}}
+
+	syd1 := &Edge{id: "BNE", weight: 9}
+	syd2 := &Edge{id: "MEL", weight: 12}
+	syd3 := &Edge{id: "CBR", weight: 4}
+	syd := &Vertex{id: "SYD", edges: Edges{syd1, syd2, syd3}}
+
+	cbr1 := &Edge{id: "MEL", weight: 6}
+	cbr2 := &Edge{id: "SYD", weight: 4}
+	cbr3 := &Edge{id: "ASP", weight: 15}
+	cbr := &Vertex{id: "CBR", edges: Edges{cbr1, cbr2, cbr3}}
+
+	mel1 := &Edge{id: "SYD", weight: 12}
+	mel2 := &Edge{id: "CBR", weight: 6}
+	mel3 := &Edge{id: "ADL", weight: 8}
+	mel := &Vertex{id: "MEL", edges: Edges{mel1, mel2, mel3}}
+
+	adl1 := &Edge{id: "MEL", weight: 8}
+	adl2 := &Edge{id: "ASP", weight: 15}
+	adl3 := &Edge{id: "PER", weight: 32}
+	adl := &Vertex{id: "ADL", edges: Edges{adl1, adl2, adl3}}
+
+	per1 := &Edge{id: "ADL", weight: 32}
+	per2 := &Edge{id: "DRW", weight: 48}
+	per := &Vertex{id: "PER", edges: Edges{per1, per2}}
+	vertices := make(map[string]*Vertex)
+	vertices[drw.id] = drw
+	vertices[cns.id] = cns
+	vertices[asp.id] = asp
+	vertices[bne.id] = bne
+	vertices[syd.id] = syd
+	vertices[cbr.id] = cbr
+	vertices[mel.id] = mel
+	vertices[adl.id] = adl
+	vertices[per.id] = per
+	g := &Graph{Vertices: vertices}
+	return g
 }
